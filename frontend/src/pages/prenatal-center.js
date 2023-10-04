489ee9ec498a02aa47f8b56178af13b1983a8aa2
Jhenil Parihar <jhenilparihar@gmail.com>
2023-10-04 12:05:30 +0530

    initial

---
+++ frontend/src/pages/prenatal-center.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrenatalCenter = () => {
+  return (
+    <div className="p-[15px]">
+      <h1 className="text-2xl font-bold">Nearby Prenatal Center</h1>
+    </div>
+  );
+};
+
+export default PrenatalCenter;
