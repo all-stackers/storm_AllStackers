f9b52e799cc1e5e839c32e3b9472a227f7056082
Jhenil Parihar <jhenilparihar@gmail.com>
2023-10-04 13:24:12 +0530

    commit

---
+++ frontend/src/utils/prenatalCenters.js
@@ -0,0 +1,58 @@
+const prenatalCenters = [
+  {
+    name: "Healthy Beginnings Prenatal Clinic",
+    address: "123 Main Street, Cityville, USA",
+    distance: "1.2 miles",
+    ratings: 4.8,
+  },
+  {
+    name: "New Life Maternity Center",
+    address: "456 Elm Avenue, Townsville, USA",
+    distance: "2.5 miles",
+    ratings: 4.5,
+  },
+  {
+    name: "Blossom Maternal Care Center",
+    address: "789 Oak Road, Villageton, USA",
+    distance: "0.8 miles",
+    ratings: 4.9,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+  {
+    name: "Miracle Moments Prenatal Clinic",
+    address: "567 Pine Street, Hamletville, USA",
+    distance: "3.0 miles",
+    ratings: 4.7,
+  },
+];
+
+export default prenatalCenters;
